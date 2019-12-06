b7e982b1581ca23c6fafe9437192e887d852de55
Minindu Hewawasam <w1698509@my.westminster.ac.uk>
2019-12-07 01:48:34 +0530

    Geetha_4.3 Sign Up Form Email availability validation

--- src/main/resources/static/js/signup_validation.js
+++ src/main/resources/static/js/signup_validation.js
@@ -3,7 +3,7 @@ function signUpValidations() {
     if (this.usernameValidation() && this.fNameValidation() && this.lNameValidation() && this.eMailValidation() && this.dobValidation() && this.passwordValidation() && this.confirmPasswordValidation() && this.mobileNoValidation() && this.genderValidation()){
         return true
     }
-        return false
+    return false
 }
 
 
@@ -15,7 +15,7 @@ function signUpValidations() {
 function usernameValidation() {
     var input= $("#inputUname").val();
     if (!input) {
-        $("#messageUName").text("Please enter your username");
+        $("#messageUName").text("Please Enter Your Username");
         return false
     }
     else{
@@ -31,7 +31,7 @@ function usernameValidation() {
 function fNameValidation() {
     var input= $("#inputFname").val();
     if (!input) {
-        $("#messageFName").text("Please enter your first name");
+        $("#messageFName").text("Please Enter Your First Name");
         return false
     }
     else{
@@ -47,7 +47,7 @@ function fNameValidation() {
 function lNameValidation() {
     var input= $("#inputLname").val();
     if (!input) {
-        $("#messageLName").text("Please enter your last name");
+        $("#messageLName").text("Please Enter Your Last Name");
         return false
     }
     else{
@@ -63,8 +63,9 @@ function lNameValidation() {
 function eMailValidation() {
     var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var input= $("#inputEmailSignUp").val();
+    checkAvailability();
     if (!input) {
-        $("#messageEMail").text("Please enter your e-mail");
+        $("#messageEMail").text("Please Enter Your E-mail");
         return false
     }
     else if(!regEx.test(String( $("#inputEmailSignUp").val()).toLowerCase())){
@@ -85,7 +86,7 @@ function eMailValidation() {
 function dobValidation() {
     var input= $("#inputDob").val();
     if (!input) {
-        $("#messageDOB").text("Please enter your date of birth");
+        $("#messageDOB").text("Please Enter Your Date of Birth");
         return false
     }
     else{
@@ -101,14 +102,12 @@ function dobValidation() {
 function passwordValidation() {
     var input= $("#inputPasswordSignUp").val();
     if (!input) {
-        $("#messagePassword").text("Please enter your password");
+        $("#messagePassword").text("Please Enter Your Password");
         return false;
     }
     else if ( input.length < 6){
-        $("#messagePassword").text("password should have at least 6 characters");
+        $("#messagePassword").text("Password Should Have at Least 6 Characters");
         return false
-    }else if(($("#inputPasswordSignUp").val())!=($("#inputConfirmPasswordSignUp").val())){
-        $("#messagePassword").text("confirm password does not match");
     }
     else{
         $("#messagePassword").text("");
@@ -123,10 +122,11 @@ function passwordValidation() {
 function confirmPasswordValidation() {
     var input= $("#inputConfirmPasswordSignUp").val();
     if (!input) {
-        $("#messageConfirmPassword").text("Please confirm your password");
+        $("#messageConfirmPassword").text("Please Confirm Your Password");
         return false
-    }
-    else{
+    }else if(($("#inputPasswordSignUp").val())!=($("#inputConfirmPasswordSignUp").val())){
+        $("#messageConfirmPassword").text("Confirm Password Does Not Match");
+    }else{
         $("#messageConfirmPassword").text("");
         return true
     }
@@ -139,13 +139,13 @@ function confirmPasswordValidation() {
 function mobileNoValidation() {
     var input= $("#inputMobile").val();
     if (!input) {
-        $("#messageMobile").text("Please enter your mobile number");
+        $("#messageMobile").text("Please Enter Your Mobile Number");
         return false
     }else if (isNaN(input)){
-        $("#messageMobile").text("invalid mobile number");
+        $("#messageMobile").text("Invalid Mobile Number");
         return false
     } else if(input.length != 10){
-        $("#messageMobile").text("Invalid mobile number");
+        $("#messageMobile").text("Mobile Number Must Have 10 digits");
         return false
     }
     else{
@@ -161,11 +161,30 @@ function mobileNoValidation() {
 function genderValidation() {
     var inputGender = $('input[name=gender]:checked').val();
     if (!inputGender) {
-        $("#messageGender").text("Please enter your gender");
+        $("#messageGender").text("Please Enter Your Gender");
         return false
     }
     else{
         $("#messageGender").text("");
         return true
     }
+}
+
+function checkAvailability() {
+    jQuery.ajax({
+        url: "/user/verifyEmail",
+        data:'email='+$("#inputEmailSignUp").val(),
+        type: "POST",
+        dataType: "json",
+        success:function(data){
+            if (data.successOrFail === "failed") {
+                $("#messageEMail").text(data.message);
+
+                return false
+            }
+            console.log(data.successOrFail);
+
+        },
+        error:function (){}
+    });
 }
