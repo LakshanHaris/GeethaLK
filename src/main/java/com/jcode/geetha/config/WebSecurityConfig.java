1044fc92568adb82e434b8076c001dc041357cf3
Lakshan <laksh777.fiverr@gmail.com>
2020-01-14 22:34:55 +0530

    Geetha_3.6 : Spring security issue fixed

--- src/main/java/com/jcode/geetha/config/WebSecurityConfig.java
+++ src/main/java/com/jcode/geetha/config/WebSecurityConfig.java
@@ -2,13 +2,17 @@ package com.jcode.geetha.config;
 
 import com.jcode.geetha.util.RequestEndPoints;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -18,7 +22,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/" + RequestEndPoints.GET_SIGN_IN_PAGE, "/").permitAll()
+                .authorizeRequests()
+                .antMatchers("/" + RequestEndPoints.GET_SIGN_IN_PAGE, "/").permitAll()
                 .antMatchers("/" + RequestEndPoints.VERIFY_EMAIL, "/").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
